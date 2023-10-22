a1f3c6afe9ec55b43c7f3cd19c0eb4b85fcb4d12
davidcnwokoro <davidcnwokoro@gmail.com>
2023-10-22 17:12:58 -0300

    added multiply function

--- project_root/basics.py
+++ project_root/basics.py
@@ -8,7 +8,10 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(name)s:%(asctime
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
 
 logging.warning(f"function to add two numbers: add(5, 7) -> {add(5, 7)}")
 
 logging.info(add(3, 4))
+
